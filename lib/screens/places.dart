dcf8031cfead14c9d084064aa4c1bf66c5a3bbeb
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-09-05 16:37:17 -0600

    Solution Refactor from video 239: Finished pulling out places list into it's own widget.

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,5 +1,6 @@
 import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/screens/new_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/providers/places_provider.dart';
@@ -24,45 +25,11 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
     if (newPlace == null) {
       return;
     }
-    // setState(() {
-    //   _groceryItems.add(newPlace);
-    // });
   }
 
   @override
   Widget build(BuildContext context) {
     final places = ref.watch(favoritePlacesProvider);
-    print('Places:');
-    print(places);
-
-    Widget content = Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Uh oh ... nothing here!',
-            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-          ),
-        ],
-      ),
-    );
-
-    if (places.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: places.length,
-        itemBuilder: (ctx, index) => Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-          child: Text(
-            places[index].title,
-            style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-          ),
-        ),
-      );
-    }
 
     return Scaffold(
       appBar: AppBar(
@@ -74,7 +41,7 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
           )
         ],
       ),
-      body: content,
+      body: PlacesList(places: places),
     );
   }
 }
---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,40 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'No Places added yet.',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
+        title: Text(
+          places[index].title,
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium!
+              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ),
+    );
+  }
+}
