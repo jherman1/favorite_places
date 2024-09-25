2467ed934eff52551670478dbd7b10e0302c6726
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-09-24 20:15:56 -0600

    Partial work for adding a 'Map' screen and being able to select a position (if one isn't already selected) from the map view.  Not fully implemented, as still haven't set up a Google Map api key.

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -23,6 +23,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   final googleMapsFullyImplemented = false;
 
   @override
@@ -41,6 +43,11 @@ class _MapScreenState extends State<MapScreen> {
       ),
       body: googleMapsFullyImplemented
           ? GoogleMap(
+              onTap: (position) {
+                setState(() {
+                  _pickedLocation = position;
+                });
+              },
               initialCameraPosition: CameraPosition(
                 target: LatLng(
                   widget.location.latitude,
@@ -48,13 +55,15 @@ class _MapScreenState extends State<MapScreen> {
                 ),
                 zoom: 16,
               ),
-              markers: {
+              markers: (_pickedLocation == null && widget.isSelecting) ? {} : {
                 Marker(
                   markerId: const MarkerId('m1'),
-                  position: LatLng(
-                    widget.location.latitude,
-                    widget.location.longitude,
-                  ),
+                  position: _pickedLocation != null
+                      ? _pickedLocation!
+                      : LatLng(
+                          widget.location.latitude,
+                          widget.location.longitude,
+                        ),
                 ),
               },
             )
