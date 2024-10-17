46b944c42a8974c3b9695349d836b63f571e99ae
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-10-16 19:12:57 -0600

    Implemented ability to save data locally on the device and access when app is re-opened.

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -4,9 +4,44 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+
+  /// Finds the database (if it exists), and calls onCreate if it does not exist (first time).
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng, REAL, address TEXT)');
+    },
+    version: 1,
+  );
+
+  return db;
+}
 
 class FavoritePlacesNotifier extends StateNotifier<List<Place>> {
   FavoritePlacesNotifier() : super(const []);
+
+  Future<void> loadFavoritePlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data.map(
+      (row) => Place(
+          id: row['id'] as String,
+          title: row['title'] as String,
+          image: File(row['image'] as String),
+          location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String)),
+    ).toList();
+
+    state = places;
+  }
 
   void addFavoritePlace(
       String title, File image, PlaceLocation location) async {
@@ -16,6 +51,21 @@ class FavoritePlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
+
+    final db = await _getDatabase();
+
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+    );
+
     state = [newPlace, ...state];
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -16,9 +16,8 @@ class PlacesScreen extends ConsumerStatefulWidget {
 }
 
 class _PlacesScreenState extends ConsumerState<PlacesScreen> {
-
-  /// Following Solution Video 240, implemented the anonymous function, in build(), 
-  /// below to use screens/add_place.dart instead of my self implemented 
+  /// Following Solution Video 240, implemented the anonymous function, in build(),
+  /// below to use screens/add_place.dart instead of my self implemented
   /// screens/new_place.dart which is called in this function.
   // @Deprecated('No longer used.  Using screens/add_place.dart instead.')
   // void _addNewPlace() async {
@@ -32,6 +31,15 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
   //   }
   // }
 
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture =
+        ref.read(favoritePlacesProvider.notifier).loadFavoritePlaces();
+  }
+
   @override
   Widget build(BuildContext context) {
     final places = ref.watch(favoritePlacesProvider);
@@ -43,13 +51,24 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
           IconButton(
             // onPressed: _addNewPlace, ///Removed b/c of explanation above ^^
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()));
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()));
             },
             icon: const Icon(Icons.add),
           )
         ],
       ),
-      body: PlacesList(places: places),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(places: places),
+          )),
     );
   }
 }
